9c59a9210d66ed702f64e8ac4ced01e7321748b8
Lucas Tavares Freire <freirelts@gmail.com>
2026-07-08 00:31:19 -0300

    first commit

---
+++ src/main/java/com/freirelts/araripe_invest_api/AraripeInvestApiApplication.java
@@ -0,0 +1,13 @@
+package com.freirelts.araripe_invest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AraripeInvestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AraripeInvestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freirelts/araripe_invest_api/AraripeInvestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freirelts.araripe_invest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AraripeInvestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
